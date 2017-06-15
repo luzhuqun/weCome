659dc9375451de3e9a5f91149d7a99d08e7f3069
lzq <595500783@qq.com>
2017-06-15 16:45:23 +0800

    first commit

---
+++ app/Models/Permission.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2017/6/15
+ * Time: 10:48
+ */
+
+namespace App;
+
+use Zizaco\Entrust\EntrustRole;
+
+class Permission extends EntrustRole
+{
+
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2017/6/15
+ * Time: 10:47
+ */
+
+namespace App;
+
+use Zizaco\Entrust\EntrustRole;
+
+class Role extends EntrustRole
+{
+
+}
